500e9c42e767c3fb40006ef67cf79cf4a8980f8c
sadawi <hahli144@hotmail.com>
2020-03-31 19:27:26 +0300

    add lesson-06

---
+++ lesson-06/sdl.h
@@ -0,0 +1,31 @@
+
+#ifndef SDL_H
+# define SDL_H
+# define SCREEN_WIDTH 640
+# define SCREEN_HEIGHT 480
+
+# include <SDL2/SDL.h>
+# include <SDL2/SDL_image.h>
+# include "../libft/includes/libft.h"
+
+enum				e_key_surfaces
+{
+	KEY_SURFACE_DEFAULT,
+	KEY_SURFACE_UP,
+	KEY_SURFACE_DOWN,
+	KEY_SURFACE_LEFT,
+	KEY_SURFACE_RIGHT,
+	KEY_SURFACE_TOTAL
+};
+
+typedef struct		s_sdl
+{
+	SDL_Window		*window;
+	SDL_Surface		*screen;
+	SDL_Surface		*image;
+	SDL_Event		e;
+	SDL_Surface		*key_press_surfaces[KEY_SURFACE_TOTAL];
+	int				img_flags;
+}					t_sdl;
+
+#endif
