678b39a3718f190cee4aecc19fa1089cc0d048e6
sadawi <hahli144@hotmail.com>
2020-03-31 16:40:24 +0300

    remove unused enum

--- lesson-03/sdl.h
+++ lesson-03/sdl.h
@@ -7,23 +7,12 @@
 #include <SDL2/SDL.h>
 #include "../libft/includes/libft.h"
 
-enum				key_surfaces
-{
-	KEY_SURFACE_DEFAULT,
-	KEY_SURFACE_UP,
-	KEY_SURFACE_DOWN,
-	KEY_SURFACE_LEFT,
-	KEY_SURFACE_RIGHT,
-	KEY_SURFACE_TOTAL
-};
-
 typedef struct		s_sdl
 {
 	SDL_Window		*window;
 	SDL_Surface		*screen;
 	SDL_Surface		*image;
 	SDL_Event		e;
-	SDL_Surface		*key_press_surfaces[KEY_SURFACE_TOTAL];
 }					t_sdl;
 
 #endif
