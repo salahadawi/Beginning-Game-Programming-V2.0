74e26fb642a7408c34b97ffccbd0c462ae439152
sadawi <hahli144@hotmail.com>
2020-04-01 16:26:38 +0300

    add lesson-11

---
+++ lesson-11/sdl.h
@@ -0,0 +1,42 @@
+
+#ifndef SDL_H
+# define SDL_H
+# define SCREEN_WIDTH 640
+# define SCREEN_HEIGHT 480
+# define TEXTURES_AMOUNT 1
+
+# include <SDL2/SDL.h>
+# include <SDL2/SDL_image.h>
+# include "../libft/includes/libft.h"
+
+enum				e_key_surfaces
+{
+	KEY_SURFACE_DEFAULT,
+	KEY_SURFACE_UP,
+	KEY_SURFACE_DOWN,
+	KEY_SURFACE_LEFT,
+	KEY_SURFACE_RIGHT,
+	KEY_SURFACE_TOTAL
+};
+
+typedef	struct		s_texture
+{
+	SDL_Texture		*texture;
+	int				width;
+	int				height;
+}					t_texture;
+
+typedef struct		s_sdl
+{
+	SDL_Window		*window;
+	SDL_Surface		*screen;
+	SDL_Surface		*image;
+	SDL_Event		e;
+	SDL_Surface		*key_press_surfaces[KEY_SURFACE_TOTAL];
+	int				img_flags;
+	SDL_Renderer	*renderer;
+	t_texture		*textures[TEXTURES_AMOUNT];
+	SDL_Rect		sprite_clips[4];
+}					t_sdl;
+
+#endif
